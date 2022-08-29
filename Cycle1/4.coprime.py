34d8f9ccb83f3b1567412bf1af6f7dc46cb7f809
anusunny339 <61958678+anusunny339@users.noreply.github.com>
2022-08-29 12:41:35 +0530

    first commit

---
+++ Cycle1/4.coprime.py
@@ -0,0 +1,10 @@
+print('21mca006')
+print('Anu Sunny')
+
+import math
+a=int(input("enter the first num"))
+b=int(input("enter the second num"))
+if math.gcd(a,b)==1:
+    print("coprime num")
+else:
+     print("not coprime")
