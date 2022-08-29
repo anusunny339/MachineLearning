87109ece2b33ebd978addd8deed0f22363661bb2
anusunny339 <61958678+anusunny339@users.noreply.github.com>
2022-08-29 12:41:13 +0530

    first commit

---
+++ Cycle1/3.py
@@ -0,0 +1,12 @@
+print('21mca006')
+print('Anu Sunny')
+
+n1 = int(input("enter the side1:"))
+n2 = int(input("enter the side2:"))
+n3 = int(input("enter the side3:"))
+if n1 == n2 and n1 == n3:
+   print("triangle is equilatrel")
+elif n1 == n2 or n1 == n3 or n2 == n3:
+   print("triangle is isoceless")
+else:
+   print("triangle ids scalar")
