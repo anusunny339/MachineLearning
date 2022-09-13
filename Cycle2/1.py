f48a47ce89cf408aa5682d59927911bac1abab74
anusunny339 <61958678+anusunny339@users.noreply.github.com>
2022-09-13 16:04:50 +0530

    initial commit

---
+++ Cycle2/1.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+arr=np.array([
+    [
+        [1.5,2.4,4.5],[2.3,6.7,8.9]
+    ],
+    [
+        [1.1,2.1,3.1],[4.1,3.7,8.1]
+    ]
+])
+
+print(arr)
+print("dimension of array=",arr.ndim)
