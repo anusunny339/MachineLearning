ea7e93ff9ced24c53724f1eeb821f59b2cd25d3d
anusunny339 <61958678+anusunny339@users.noreply.github.com>
2023-01-05 12:08:32 +0530

    intial commit

--- knn.py
+++ knn.py
@@ -1,41 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 import sklearn as sl
 
-dataset=pd.read_csv("iris.csv")
-
-X=dataset.iloc[:,:-1].values
-y=dataset.iloc[:,4].values
-
+data=pd.read_csv("iris.csv")
+x=data.iloc[:,:-1].values
+y=data.iloc[:,4].values
 
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.20)
-
-#from sklearn.model_selection import train_test_split
-#X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.20)
-print(X)
-print(y)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20)
+print("x:",x)
+print("\ny:",y)
 
 from sklearn.neighbors import KNeighborsClassifier
 classifier=KNeighborsClassifier(n_neighbors=3)
+classifier.fit(x_train,y_train)
+y_pred=classifier.predict(x_test)
 
-#from sklearn.neighbors import KNeighborsClassifier
-#classifier=KNeighborsClassifier(n_neighbors=3)
-
-#classifier.fit(X_train,y_train)
-#y_pred=classifier.predict(X_test)
-
-classifier.fit(X_train, y_train)
-y_pred=classifier.predict(X_test)
-
-
-#from sklearn.metrics import confusion_matrix,classification_report
-
-#print(classification_report(y_test,y_pred))
-
-
-from sklearn.metrics import classification_report,confusion_matrix
-print(classification_report(y_test,y_pred))
-
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+print("matrix",confusion_matrix(y_test,y_pred))
+print("\n Classification Report",classification_report(y_test,y_pred))
+print("\naccuracy:",accuracy_score(y_test,y_pred))
 
